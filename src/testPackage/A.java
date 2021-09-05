b21a8c0e19b0db23e69b6f58088c539a0f789bd1
Arundhathi <arunap2007@gmail.com>
2021-09-05 20:51:55 +0530

    updated in class A

--- src/testPackage/A.java
+++ src/testPackage/A.java
@@ -4,7 +4,9 @@ public class A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+       System.out.println("Iam in Class A");
+       System.out.println("Class A");
+       
 	}
 
 }
